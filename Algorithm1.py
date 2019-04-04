79fe706f4d8f29d2d35d9a06ecfeff08ba528a39
Artificial Intelligence Initiative <31492730+ArtificialIntelligenceInitiative@users.noreply.github.com>
2019-04-04 17:40:33 -0500

    Algorithm with Wiki

--- Algorithm1.py
+++ Algorithm1.py
@@ -1,15 +1,26 @@
 import spacy
+import wikipedia
+G =wikipedia.summary("Facebook")
+Sentences = []
+reshaping = []
+increment = 0
+for word in G.split("."):
+   Sentences.append(word)
+ #print(Sentences)
+print(Sentences)
 nlp = spacy.load('en')
-Sentences = ["The students are upstairs.", "The students are diligent.", "The students are scholars."]
+
+doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
 Dictionary={}
 sentence_Counter = 0
+Sentences=["the cat caught a mouse"]
 test = 1
 for sentence in Sentences:
   sentence_Counter = sentence_Counter+1 
   doc = nlp(sentence)
   checking = 0
   for token in doc:
-    print(token.text, token.pos_, token.dep_)
+   # print(token.text, token.pos_, token.dep_)
     try:
       if token.dep_ not in Dictionary[token.pos_]:
         Dictionary[token.pos_].append(token.dep_)
